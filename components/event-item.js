c2bcb9664eb15299b3e88e60ddb71583fdca2cba
Jyoti Gupta <jyotiguptaofficial@gmail.com>
2023-06-21 14:06:22 +0530

    add the icon on the event list component

--- components/event-item.js
+++ components/event-item.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import classes from './event-item.module.css'
 import Button from "@/components/ui/button";
+import DateIcon from "@/components/icons/date-icon";
+import AddressIcon from "@/components/icons/address-icon";
+import ArrowRightIcon from "@/components/icons/arrow-right-icon";
 
 function EventItem(props) {
     const {title, image, date, location, id} = props;
@@ -18,17 +21,23 @@ function EventItem(props) {
                 <h2>{title}</h2>
             </div>
             <div className={classes.date}>
+                <DateIcon/>
                 <time>
                     {readableDate}
                 </time>
             </div>
             <div className={classes.address}>
+                <AddressIcon/>
                 <address>
                     {formattedAddress}
                 </address>
             </div>
             <div className={classes.actions}>
-                <Button link={exploreLink}>Explore Event</Button>
+                <Button link={exploreLink}><span>
+                    Explore Event
+                </span>
+                    <span className={classes.icon}><ArrowRightIcon/></span>
+                </Button>
             </div>
         </div>
 
